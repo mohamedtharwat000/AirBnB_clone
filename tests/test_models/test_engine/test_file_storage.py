d0997c89f95550953d1d18c295fa22053d5f8271
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-13 01:24:17 +0300

    test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,20 +7,26 @@ from models.base_model import BaseModel
 import unittest
 import json
 from os.path import exists
+from os import remove
 
 
 class TestFileStorage(unittest.TestCase):
     """Unit tests for the FileStorage Class"""
 
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         """Set up test environment"""
-        cls.fs = FileStorage()
+        self.fs = FileStorage()
+        self.bm = BaseModel()
+        self.fs.new(self.bm)
+
+    def tearDown(self):
+        """Tear down test environment"""
+        del self.fs
+        del self.bm
 
     def test_new_instance(self):
         """Test creating a new instance of FileStorage"""
         self.assertIsInstance(self.fs, FileStorage)
-        self.assertTrue(self.fs.all(), {})
 
     def test_all(self):
         """Test all method"""
@@ -28,14 +34,10 @@ class TestFileStorage(unittest.TestCase):
 
     def test_new(self):
         """Test the 'all' method and creating new objects"""
-        self.bm = BaseModel()
-        self.fs.new(self.bm)
         self.assertEqual(self.fs.all()[f"BaseModel.{self.bm.id}"], self.bm)
 
     def test_save(self):
         """Test the 'save' method and JSON file creation"""
-        self.bm = BaseModel()
-        self.fs.new(self.bm)
         self.fs.save()
         self.assertTrue(exists("./file.json"))
         with open("./file.json", "r") as file:
@@ -44,8 +46,6 @@ class TestFileStorage(unittest.TestCase):
 
     def test_reload(self):
         """Test the 'reload' method"""
-        self.bm = BaseModel()
-        self.fs.new(self.bm)
         self.fs.save()
         self.fs.reload()
         self.assertEqual(self.fs.all()[f"BaseModel.{self.bm.id}"].id,
